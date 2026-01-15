776aca1150966bafdd2e464e6a8390191d394258
ludhfiansyah <ludhfiansyah@gmail.com>
2026-01-15 16:33:48 +0700

    feat: add task 10-javascriptNumber

---
+++ 10-JavaScript-Number/task.js
@@ -0,0 +1,11 @@
+// 1. Declare and initialise a variable with the value of 100,000, log this to the console.
+let pajak = 1e6;
+console.log(pajak);
+
+// 2. In the console, identify the data type of the variable
+console.log(typeof pajak);
+
+// 3. Create a float data type, and log this type to the console.
+let floatData = 1e-6;
+console.log(typeof floatData);
+console.log(floatData);
