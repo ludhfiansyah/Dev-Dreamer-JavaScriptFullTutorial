624cfac5a85f82ec2c13a5991102ad430955ee12
ludhfiansyah <ludhfiansyah@gmail.com>
2026-01-16 11:37:17 +0700

    feat: add task 18-JavaScript-Comparion-Operators

---
+++ 18-JavaScript-Comparion-Operators/task.js
@@ -0,0 +1,30 @@
+/*
+1. Create two variables, called a and b. Assign the value 10 to a
+   and 15 to b. Check to see if a is more than b, log the answer to the
+   console.
+*/
+const a = 10;
+const b = 15;
+console.log(a > b);
+
+/*
+2. Use the relevant comparison operator to identify whether a is
+   equal to "10". Log your answer to the console.
+*/
+console.log(a == "10");
+
+/*
+3. Check once again to see if a is equal to "10", this time making
+   sure to check against the type as well. Log your answer to the
+   console.
+*/
+console.log(a === "10");
+
+/*
+4. If x is equal to "5" and y is equal to 6, what would the result
+   of the following be?
+   x !== y?
+*/
+const x = "5";
+const y = 6;
+console.log(x !== y);
