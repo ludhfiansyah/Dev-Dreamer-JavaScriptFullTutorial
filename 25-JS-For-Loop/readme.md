dbee696d13776fee1f01667efb84e53fb0ab2dac
ludhfiansyah <ludhfiansyah@gmail.com>
2026-01-17 13:48:31 +0700

    feat: add task 25-JS-For-Loop

---
+++ 25-JS-For-Loop/readme.md
@@ -0,0 +1,4 @@
+# TASKS
+
+1. Create an array of five colors, use a for loop to iterate over each color
+   with the statement: '[color] is my fav color!' Console log the results.
---
+++ 25-JS-For-Loop/task.js
@@ -0,0 +1,9 @@
+/*
+1. Create an array of five colors, use a for loop to iterate over each color
+   with the statement: '[color] is my fav color!' Console log the results.
+*/
+const colors = ["white", "red", "blue", "yellow", "black"];
+for (let i = 0; i < colors.length; i++) {
+  let info = `${colors[i]} is my fav color!`;
+  console.log(info);
+}
